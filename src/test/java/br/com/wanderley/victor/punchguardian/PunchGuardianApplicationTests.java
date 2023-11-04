a934a84077904c4384e3045cb8507d49e72ffb31
Victor Wanderley <victorwasouza@gmail.com>
2023-11-04 00:29:12 -0300

    Build: Criando Arquivos e Configurações Iniciais

---
+++ src/test/java/br/com/wanderley/victor/punchguardian/PunchGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wanderley.victor.punchguardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PunchGuardianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
