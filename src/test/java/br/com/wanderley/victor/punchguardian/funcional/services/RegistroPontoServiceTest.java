a1dcbfbff700bf2c239f4e7ea1ab8b7ebc98e793
Victor Wanderley <victorwasouza@gmail.com>
2023-12-22 20:42:33 -0300

    PG-2 draft: Adicionando testes e nova Exception

--- src/test/java/br/com/wanderley/victor/punchguardian/funcional/services/RegistroPontoServiceTest.java
+++ src/test/java/br/com/wanderley/victor/punchguardian/funcional/services/RegistroPontoServiceTest.java
@@ -2,6 +2,7 @@ package br.com.wanderley.victor.punchguardian.funcional.services;
 
 import br.com.wanderley.victor.punchguardian.comum.models.dtos.MensagemRetornoDTO;
 import br.com.wanderley.victor.punchguardian.comum.models.entities.Pessoa;
+import br.com.wanderley.victor.punchguardian.funcional.exceptions.EspelhoPontoException;
 import br.com.wanderley.victor.punchguardian.funcional.models.Cargo;
 import br.com.wanderley.victor.punchguardian.funcional.models.Profissional;
 import br.com.wanderley.victor.punchguardian.funcional.models.RegistroPonto;
@@ -9,15 +10,18 @@ import br.com.wanderley.victor.punchguardian.funcional.models.dtos.response.espe
 import br.com.wanderley.victor.punchguardian.funcional.models.enums.TipoPonto;
 import br.com.wanderley.victor.punchguardian.funcional.repositories.ProfissionalRepository;
 import br.com.wanderley.victor.punchguardian.funcional.repositories.RegistroPontoRepository;
+import org.hibernate.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -63,17 +67,38 @@ public class RegistroPontoServiceTest {
     @Test
     public void registrarPontoErroProfissionalNaoEncontrado(){
         when(profissionalRepository.findById(profissional.getId())).thenReturn(Optional.empty());
-        assertThrows(RuntimeException.class, () -> pontoService.registrarPonto(profissional.getId()), "Id Profissional "+profissional.getId()+" não existe");
+        assertThrows(ObjectNotFoundException.class, () -> pontoService.registrarPonto(profissional.getId()), "Id Profissional "+profissional.getId()+" não existe");
         verify(profissionalRepository, times(1)).findById(profissional.getId());
     }
 
     @Test
-    void testEspelhoDePontoSemIntervalo() {
+    void testEspelhoDePontoSemIntervaloRetornoVazio() {
         when(profissionalRepository.findById(any())).thenReturn(Optional.of(profissional));
         when(pontoRepository.findByProfissionalOrderByHoraAsc(any())).thenReturn(Collections.emptyList());
 
         EspelhoPontoDTO espelho = pontoService.espelhoDePonto(profissional.getId());
         assertNotNull(espelho);
     }
+
+    @Test
+    void testEspelhoDePontoSemIntervaloRetornoEspelho() {
+        List<RegistroPonto> pontos = List.of(new RegistroPonto(profissional, LocalDateTime.now().minusHours(1), TipoPonto.ENTRADA),
+                new RegistroPonto(profissional, LocalDateTime.now(), TipoPonto.SAIDA));
+        when(profissionalRepository.findById(any())).thenReturn(Optional.of(profissional));
+        when(pontoRepository.findByProfissionalOrderByHoraAsc(profissional)).thenReturn(pontos);
+
+        EspelhoPontoDTO espelho = pontoService.espelhoDePonto(profissional.getId());
+        assertNotNull(espelho);
+        assertEquals(Duration.ofHours(1).toHours(),espelho.getTotalHorasTrabalhadas().toHours());
+    }
+
+    @Test
+    void testEspelhoPontoDataInicioMaiorDataFinal(){
+        when(profissionalRepository.findById(any())).thenReturn(Optional.of(profissional));
+        assertThrows(EspelhoPontoException.class,
+                () -> pontoService.espelhoDePonto(profissional.getId(), LocalDate.now(), LocalDate.now().minusDays(1)),
+                "A data final é anterior a data inicial");
+    }
+
 
 }
