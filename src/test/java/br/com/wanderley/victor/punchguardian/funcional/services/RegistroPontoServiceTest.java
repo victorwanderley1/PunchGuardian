ce80799a489dfb764ac91d23ba64532fcf2d44df
Victor Wanderley <victorwasouza@gmail.com>
2023-12-21 22:30:07 -0300

    PG-2 test: Adicionando testes iniciais para registro de pontos e obtencao do espelho de ponto

---
+++ src/test/java/br/com/wanderley/victor/punchguardian/funcional/services/RegistroPontoServiceTest.java
@@ -0,0 +1,79 @@
+package br.com.wanderley.victor.punchguardian.funcional.services;
+
+import br.com.wanderley.victor.punchguardian.comum.models.dtos.MensagemRetornoDTO;
+import br.com.wanderley.victor.punchguardian.comum.models.entities.Pessoa;
+import br.com.wanderley.victor.punchguardian.funcional.models.Cargo;
+import br.com.wanderley.victor.punchguardian.funcional.models.Profissional;
+import br.com.wanderley.victor.punchguardian.funcional.models.RegistroPonto;
+import br.com.wanderley.victor.punchguardian.funcional.models.dtos.response.espelho.EspelhoPontoDTO;
+import br.com.wanderley.victor.punchguardian.funcional.models.enums.TipoPonto;
+import br.com.wanderley.victor.punchguardian.funcional.repositories.ProfissionalRepository;
+import br.com.wanderley.victor.punchguardian.funcional.repositories.RegistroPontoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class RegistroPontoServiceTest {
+
+    @Mock
+    private ProfissionalRepository profissionalRepository;
+
+    @Mock
+    private RegistroPontoRepository pontoRepository;
+
+    @InjectMocks
+    private RegistroPontoService pontoService;
+
+    private Pessoa pessoa = new Pessoa(1, "Testador da Silva", LocalDate.of(1990, 01, 01),
+            "Rua das Laranjas", "359.726.760-28", "emaildotestador@email.com");
+
+    private Cargo cargo = new Cargo(1,"Testador Profissional","Realizar Testes Unitários");
+
+    private Profissional profissional = new Profissional(1, cargo, 40, pessoa);
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void registrarPontoComSucesso(){
+        when(profissionalRepository.findById(profissional.getId())).thenReturn(Optional.of(profissional));
+        when(pontoRepository.getTopByProfissionalOrderByHoraDesc(profissional))
+                .thenReturn(new RegistroPonto(1L, LocalDateTime.now(), TipoPonto.ENTRADA, profissional, false));
+        when(pontoRepository.save(any(RegistroPonto.class)))
+                .thenReturn(new RegistroPonto(2L, LocalDateTime.now(), TipoPonto.SAIDA, profissional, false));
+        MensagemRetornoDTO mensagem = pontoService.registrarPonto(profissional.getId());
+        verify(profissionalRepository, times(1)).findById(profissional.getId());
+        verify(pontoRepository, times(1)).getTopByProfissionalOrderByHoraDesc(profissional);
+        assertNotNull(mensagem);
+        assertTrue(mensagem.getMensagemRetorno().startsWith("Ponto de "+TipoPonto.SAIDA+" do profissional "+pessoa.getNome()));
+    }
+
+    @Test
+    public void registrarPontoErroProfissionalNaoEncontrado(){
+        when(profissionalRepository.findById(profissional.getId())).thenReturn(Optional.empty());
+        assertThrows(RuntimeException.class, () -> pontoService.registrarPonto(profissional.getId()), "Id Profissional "+profissional.getId()+" não existe");
+        verify(profissionalRepository, times(1)).findById(profissional.getId());
+    }
+
+    @Test
+    void testEspelhoDePontoSemIntervalo() {
+        when(profissionalRepository.findById(any())).thenReturn(Optional.of(profissional));
+        when(pontoRepository.findByProfissionalOrderByHoraAsc(any())).thenReturn(Collections.emptyList());
+
+        EspelhoPontoDTO espelho = pontoService.espelhoDePonto(profissional.getId());
+        assertNotNull(espelho);
+    }
+
+}
