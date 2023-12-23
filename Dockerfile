669ed856d17970430875185bec4c9d37d18e114f
Victor Wanderley <victorwasouza@gmail.com>
2023-12-23 18:54:07 -0300

    PG-5 draft: Adicionando integracao com docker e gradle do github

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,6 @@
-# Estágio de construção
-FROM gradle:jdk17 as builder
-LABEL authors="victor.wanderley"
-WORKDIR /app
-COPY build.gradle .
-COPY settings.gradle .
-COPY src src
-RUN gradle build --no-daemon --exclude-task test
-
-# Estágio de execução
 FROM openjdk:17
 LABEL authors="victor.wanderley"
 WORKDIR /app
-COPY --from=builder /app/build/libs/PunchGuardian-1.4.1-snapshot.jar PunchGuardian.jar
+COPY /build/libs/PunchGuardian.jar PunchGuardian.jar
 EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "PunchGuardian.jar"]
--- src/test/java/br/com/wanderley/victor/punchguardian/TestPunchGuardianApplication.java
+++ src/test/java/br/com/wanderley/victor/punchguardian/TestPunchGuardianApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
