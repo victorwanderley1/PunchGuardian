b2af393fa6d57f585176f72c448df70af59b0100
Victor Wanderley <victorwasouza@gmail.com>
2023-12-24 19:20:46 -0300

    PG-10 draft: Barrando endpoints

--- src/main/java/br/com/wanderley/victor/punchguardian/security/settings/SecurityConfigurations.java
+++ src/main/java/br/com/wanderley/victor/punchguardian/security/settings/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package br.com.wanderley.victor.punchguardian.security.settings;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +16,10 @@ public class SecurityConfigurations {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/ponto").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
                 .build();
    }
 
